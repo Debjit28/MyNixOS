3c470553cc227772e196146a92951f3c5bdbbfb0
Debjit28 <debjit2807@gmail.com>
2026-04-17 11:03:39 +0530

    Feat: Added dhcp due to lab

--- modules/hosts/my-machine/configuration.nix
+++ modules/hosts/my-machine/configuration.nix
@@ -192,7 +192,7 @@
       xwayland-satellite playerctl brightnessctl cage
       papirus-icon-theme gnome-themes-extra metasploit
       nmap inetutils gvfs thunar xfce.thunar-volman 
-      xfce.thunar-archive-plugin  dhcp
+      xfce.thunar-archive-plugin  
       tumbler pavucontrol nwg-look baobab  
     ];
 
